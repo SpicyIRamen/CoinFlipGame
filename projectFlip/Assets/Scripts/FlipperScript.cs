9bf2849296f575c26404dca1e23c3d47517e7711
Sean <sean.werner@iths.se>
2021-09-13 16:53:09 +0200

    added obj that tracks touch and interacts with coin

---
+++ projectFlip/Assets/Scripts/FlipperScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetFlipperToTouch();
+    }
+
+    private void SetFlipperToTouch(){
+        var touchPos = Input.mousePosition;
+        touchPos.z = 25; //distance of 10 units from camera
+
+        rb.position = Camera.main.ScreenToWorldPoint(touchPos);
+    }
+}
--- projectFlip/Assets/Scripts/TouchManager.cs
+++ projectFlip/Assets/Scripts/TouchManager.cs
@@ -10,7 +10,6 @@ public class TouchManager : MonoBehaviour
 
     }
 
-    public Text tCount;
 
     GameObject gObj = null;
     Plane objPlane;
@@ -32,15 +31,16 @@ public class TouchManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        tCount.text = Input.touchCount.ToString();
 
-        if (Input.GetTouch(0).phase == TouchPhase.Began)
-        {
+        if (Input.touchCount > 0)
+        {       
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
             Ray mouseRay = GenerateMouseRay(Input.GetTouch(0).position);
             RaycastHit hit;
 
-            if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out hit))
-            {
+                if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out hit))
+                {
                 gObj = hit.transform.gameObject;
                 objPlane = new Plane(Camera.main.transform.forward * -1, gObj.transform.position);
 
@@ -48,18 +48,21 @@ public class TouchManager : MonoBehaviour
                 Ray mRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
                 float rayDistane;
                 objPlane.Raycast(mRay, out rayDistane);
+                }
             }
-        }
-        else if (Input.GetTouch(0).phase == TouchPhase.Moved && gObj)
-        {
+            else if (Input.GetTouch(0).phase == TouchPhase.Moved && gObj)
+            {
+
             Ray mRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             float rayDistance;
+            
             if (objPlane.Raycast(mRay, out rayDistance))
                 gObj.transform.position = mRay.GetPoint(rayDistance) + m0;
-        }
-        else if (Input.GetTouch(0).phase == TouchPhase.Ended && gObj)
-        {
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended && gObj)
+            {
             gObj = null;
+            }
         }
 
     }
